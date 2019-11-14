9652119b6458bfd9738b1e5855d61007928fef82
Pascal Dennerly <dnnrly@gmail.com>
2019-11-14 20:39:51 +0000

    Fix replacements in all locations and paths

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+while getopts “d” opt; do
+  case $opt in
+    d) ECHO=echo ;;
+  esac
+done
+
 echo "Enter your username (Github org):"
 read ORG
 
@@ -7,12 +13,20 @@ echo "Enter the name of your project:"
 read NAME
 
 set -e
-set -x
 
-for f in `find . -type f -not -iwholename '*.git*' -not -iwholename '*tmp*' -not -iwholename '*libexec*' -not -iwholename '*bin*' -not -iwholename '*share*' -not -iwholename '*setup*'`
+${ECHO} mv ./cmd/goclitem ./cmd/${NAME}
+${ECHO} mv ./cmd/${NAME}/goclitem.go ./cmd/${NAME}/${NAME}.go
+${ECHO} mv ./cmd/${NAME}/goclitem_test.go ./cmd/${NAME}/${NAME}_test.go
+${ECHO} mv ./goclitem.go ./${NAME}.go
+${ECHO} mv ./goclitem_test.go ./${NAME}_test.go
+
+update() {
+    sed -i -e "s/dnnrly/${ORG}/g" ${1}
+    sed -i -e "s/goclitem/${NAME}/g" ${1}
+}
+
+for f in `find . -type f -not -iwholename '*.git/*' -not -iwholename '*tmp*' -not -iwholename '*libexec*' -not -iwholename '*bin*' -not -iwholename '*share*' -not -iwholename '*setup*'`
 do
-    sed -i -e "s/dnnrly/${ORG}/g" $f
-    sed -i -e "s/goclitem/${NAME}/g" $f
+    ${ECHO} update $f
 done
 
-mv ./cmd/goclitem ${NAME}
